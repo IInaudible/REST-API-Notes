5bdd1c20908c347aa3d9cc8f28ab501a2d14ee48
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-05-05 14:31:09 -0700

    instructions for app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+//open terminal
+//type npm init -y
+//type npm install express
+//create a variable called express and require 'express'
+//create a variable called app and set it equal to express()
+//create a variable called port and set it equal to 3000
+//Set up a post route for '/api/note' and use res.send to send a string
+//Set up a get route for '/api/note/:uid' and use res.send to send a string
+//Set up a put route for 'api/note/:uid' and use res.send to send a string
+//Set up a delete route for 'api/note/:uid' and use res.send to send a string
+//Set up a get route for '/api/notes' and use res.send to send a string
+//make sure that your app is listening on the port
+//console.log `app listening on ${port}
+//run the app from the terminal (node app.js)
